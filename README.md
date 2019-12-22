284db56117e056d85a9bec228211d6d7e77dab53
manolin.martin <mjmm_manuel@hotmail.com>
2019-12-22 23:54:29 +0100

    add  nsmallest and sort_values

--- README.md
+++ README.md
@@ -9,3 +9,25 @@ In the Data Processing context with Pandas Python library, we deep in some inter
 Return Not equal to of series and other, element-wise (binary operator ne).
 
 But not only we can use this logical, we can use this binary operators **eq**, **ne**, **le**, **lt**, **ge**, **gt**.
+
+## 1.2. [nsmallest()](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.nsmallest.html#pandas.DataFrame.nsmallest)
+
+Return the first n rows ordered by columns in ascending order.
+
+Of course we can obtain its homologue.
+
+## 1.3. [sort_values()](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.sort_values.html#pandas.DataFrame.sort_values)
+
+Sort by the values along either axis.
+
+There are some options:
+
+1. Inplace
+2. Kind: [quicksort, mergesort, heapsort]
+3. NA
+
+## 1.4. [pdpip](https://pdpipe.github.io/pdpipe/doc/pdpipe/):
+
+The **pdpip** library, [website](https://github.com/pdpipe/pdpipe) repository.
+
+
--- main.py
+++ main.py
@@ -4,9 +4,27 @@
 import pandas as pd
 
 # Example ne():
-money = [0, 0, 0, 2, 3, 0, 1, 1, 3, 5, 0]
-example_money = pd.DataFrame({'money': money})
-print(example_money)
+money = [0, 0, 1, 2, 3, 0, 1, 1, 3, 5, 0]
+user = ["name_b", "name_b", "name_a", "name_b", "name_b", "name_b", "name_b", "name_c", "name_a", "name_a", "name_a"]
+
+example = pd.DataFrame({'name':user,
+                              'money': money})
+
+print(example)
 print('Example operator with ne:')
-example_filtered = example_money.loc[example_money['money'].ne(0).idxmax():]
+example_filtered = example.loc[example['money'].ne(0).idxmax():]
 print(example_filtered)
+
+# Example nsmallest():
+example_nsmallest_first = example.nsmallest(n=5, columns=['money'], keep='first')
+print('Example n-smallest with keep first')
+print(example_nsmallest_first)
+
+example_nsmallest_last = example.nsmallest(n=5, columns=['money'], keep='last')
+print('Example n-smallest with keep last')
+print(example_nsmallest_last)
+
+# Example sort_values():
+example_sorted = example.sort_values(by=['name', 'money'], inplace=False)
+print('Example sort_values')
+print(example_sorted)
